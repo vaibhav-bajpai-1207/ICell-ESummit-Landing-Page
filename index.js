2f91942e025801d7cb831f5ae657583fa3c196fe
Vaibhav <vaibhav.bajpai1221@gmail.com>
2022-08-31 23:20:49 +0530

    Made the hero section and the speaker section base. Work is still needed on Speaker Section

---
+++ index.js
@@ -0,0 +1,19 @@
+let navbar = document.getElementById('nav');
+let speakerThumb = document.querySelectorAll('.speaker-thumb');
+let speakerThumbOverlay = document.querySelectorAll('')
+
+let navBg = ()=>{
+    if(window.scrollY != 0){
+        navbar.style.backgroundColor = "rgba(0, 0, 0, 0.94)";
+    }else{
+        navbar.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    }
+}
+
+let showSpeakerDetail = ()=>{
+    speakerThumb.forEach(ele=>{
+        ele.childNodes[]
+    })
+}
+
+addEventListener('scroll', navBg);
